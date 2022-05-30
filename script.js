e47f214eae8cdc1f591093cdb7d9c4f79eb2fb8b
andrea scacchi <andrea.scacchi@live.com>
2022-05-30 05:20:03 +0200

    Completed Milestone 1 and 2

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
 // script for Exercise
 
 const mytoDoList = new Vue({
-    el:"root",
+    el:"#root",
     data : {
+        toDos : [
+            {
+                "text"   : "Ehi prof che cosa vuoi fare stasera?",
+                "done" : true 
+            },
+            {
+                "text"   : "Quello che facciamo tutte le sere Mignolo..",
+                "done" : false 
+            },
+            {
+                "text"   : "Tentare di conquistare il mondo!",
+                "done" : true 
+            },
+            {
+                "text"   : "(cantante) Il mignolo col prof! il mignolo col prof!",
+                "done" : false
+            },           
+        ]
 
     },
     methods : {
-
+        // delete an element on click of another
+        deleteToDo(index) {
+            this.toDos.splice(index,1);
+        }
     }
 });
